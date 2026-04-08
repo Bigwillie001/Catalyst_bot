eba2b6eb50f0b9863a92dbf05d08a4b629d80055
Bigwillie001 <davidtestimony183@gmail.com>
2026-04-08 12:19:03 +0100

    Fix pip path for build

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
-from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.llms.groq import GoogleGenAI
 from llama_index.core import Settings
 
-Settings.llm = GoogleGenAI(model_name="models/gemini-2.0-flash", api_key=os.getenv("GEMINI_API_KEY"))
+Settings.llm = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 CHROMA_PATH = "./storage/chroma_db"
 UPLOADS_PATH = "./storage/uploads"
 EMBED_MODEL = "all-MiniLM-L6-v2"
